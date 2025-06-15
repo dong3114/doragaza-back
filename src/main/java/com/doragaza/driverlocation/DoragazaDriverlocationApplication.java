575cf147bad903d7e2ddc45385780a12102a20fa
dong3114 <scv3114@gmail.com>
2025-06-15 22:33:19 +0900

    init: Doragaza BackEnd

---
+++ src/main/java/com/doragaza/driverlocation/DoragazaDriverlocationApplication.java
@@ -0,0 +1,13 @@
+package com.doragaza.driverlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoragazaDriverlocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoragazaDriverlocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doragaza/driverlocation/DoragazaDriverlocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doragaza.driverlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoragazaDriverlocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
